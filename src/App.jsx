3b39f8033585a05b02f732b526c14ee290aa170b
uumarbek <koko87@mail.ru>
2024-09-22 22:03:42 +0500

    home MY9

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Home from './Pages/Home/Home'
 import Layout from './LAYOUT/Layout'
+import Home from './Pages/Home/Home'
 
 function App() {
 
