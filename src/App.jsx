94c52f8b8c812161de7ee17fdc164823dead4363
kinddevs <kinddevs2024@gmail.com>
2024-12-28 22:45:35 +0500

    03

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Layout from './LAYOUT/Layout';
+import Layout from './Layout/Layout.jsx';
 import Home from './Pages/Home/Home';
 
 function App() {
